5c5e71d2989f653ab6512ed659e8ed96554504c1
victoria-mischley <137920629+victoria-mischley@users.noreply.github.com>
2024-01-18 15:44:48 -0800

    Update gen_exemplars.py

--- gen_exemplars.py
+++ gen_exemplars.py
@@ -55,7 +55,9 @@ def make_complexes(folder_path):
                 apo_file_name = file_name.split(".pdb")[0]
                 apo_file_path = f"{folder_path_path}/apo/{apo_file_name}.pdb"
                 exemplar_file_path = file
-                complex_file_path = f"{folder_path_path}/complexes/{apo_file_name}_Complex.pdb"
+                exemplar_file_name = file.name
+                exemplar_file_name_short = exemplar_file_name[:-4]
+                complex_file_path = f"{folder_path_path}/complexes/{exemplar_file_name_short}_Complex.pdb"
                 with open(apo_file_path, 'r') as f1:
                     apo_data = f1.read()
                 with open(exemplar_file_path) as f2:
